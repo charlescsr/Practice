a3c96d4e59bd0293eade7a0c6da7b75cdb387de1
CSR <51647212+charlescsr@users.noreply.github.com>
2019-09-18 20:34:45 +0530

    Basic datatypes in C++
    cout.precision(const) to set precision of float and double
    fixed-write floating-point values in fixed-point notation

---
+++ datatype.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;long l1;char c;float f;double d;
+    cin>>a>>l1>>c>>f>>d;
+    cout<<a<<endl;
+    cout<<l1<<endl;
+    cout<<c<<endl;
+    cout.precision(3);
+    cout<<fixed<<f<<endl;
+    cout.precision(9);
+    cout<<fixed<<d<<endl;
+    return 0;
+}
