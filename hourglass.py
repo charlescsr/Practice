c33d74ed9fcafb5f76ad45a0512cb97435f226e8
CSR <51647212+charlescsr@users.noreply.github.com>
2020-03-10 17:29:11 +0530

    Add hourglass.py
    Hourglass sum

---
+++ hourglass.py
@@ -0,0 +1,7 @@
+def hourglassSum(arr):
+    m=-sys.maxsize-1
+    for i in range(len(arr)-2):
+        for j in range(len(arr[0])-2):
+            m=max(m,arr[i][j]+arr[i][j+1] + arr[i][j+2]+ arr[i+1][j+1]+ arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+
+    return m
