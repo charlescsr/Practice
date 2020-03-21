2f0eb94c6aa53c2a39f16f61c0871cf8a96c22a9
CSR <51647212+charlescsr@users.noreply.github.com>
2020-03-21 21:42:45 +0530

    Add pairs.py
    Find the number of elements that equal a given number

---
+++ pairs.py
@@ -0,0 +1,21 @@
+import os
+
+def pairs(k, arr):
+    return len(set(arr) & set(x + k for x in arr))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
