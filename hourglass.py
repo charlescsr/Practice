939b182e8b260820d79ae75b7f953c84c45da527
CSR <51647212+charlescsr@users.noreply.github.com>
2020-03-25 19:35:18 +0530

    Update hourglass.py

--- hourglass.py
+++ hourglass.py
@@ -1,3 +1,4 @@
+import sys
 def hourglassSum(arr):
     m=-sys.maxsize-1
     for i in range(len(arr)-2):
@@ -5,3 +6,9 @@ def hourglassSum(arr):
             m=max(m,arr[i][j]+arr[i][j+1] + arr[i][j+2]+ arr[i+1][j+1]+ arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
 
     return m
+
+arr = []
+for arr_i in range(6):
+   arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+   arr.append(arr_t)
+hourglassSum(arr)
